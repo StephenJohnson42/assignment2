4eb3c0ca2260a5244f4e03329ecdefd2eb3e6d84
StephenJohnson42 <54747618+StephenJohnson42@users.noreply.github.com>
2019-10-04 00:20:40 -0400

    Defined Skateboard mileage Estimate

--- Skateboard.cpp
+++ Skateboard.cpp
@@ -5,8 +5,32 @@
 //
 
 #include "Skateboard.h"
+#include <stdio.h>     
+#include <stdlib.h>     
+#include <time.h>     
+#include <string>   
+#include <cmath>
+
+Skateboard::Skateboard() {
+}
 
 Skateboard::~Skateboard() = default;
+
+double Bicycle::mileageEstimate(double timex) {
+	srand (time(NULL));
+	int x = rand() % 5 + 1;
+	double y = 0;
+	double z = 10.0;
+	y = x / z;
+
+    double mileage = y * timex;
+    if(timex > 25 && timex < 250){
+    	double ranAmount = rand() % (timex / .3333) + 1;
+	    mileage = mileage + ranAmount ;
+	}
+	mileage = floor(mileage);
+    return mileage;
+}
 
 string Skateboard::toString() {
     string s = "-> Skateboard\n\t";
