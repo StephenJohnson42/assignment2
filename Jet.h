c926f0679d1fc671b5f401c431df5be128589c26
StephenJohnson42 <54747618+StephenJohnson42@users.noreply.github.com>
2019-10-03 22:59:11 -0400

    Added virtual functions to Jet header

---
+++ Jet.h
@@ -0,0 +1,20 @@
+//
+// Created by Stephen Johnson 10/3/2019
+//
+
+#ifndef DRIVINGSIMULATOR_JET_H
+#define DRIVINGSIMULATOR_JET_H
+
+#include "Vehicle.h"
+
+class Jet : public PoweredVehicle {
+
+private:
+   int numberOfEngines;
+
+public:
+
+};
+
+
+#endif //DRIVINGSIMULATOR_JET_H
